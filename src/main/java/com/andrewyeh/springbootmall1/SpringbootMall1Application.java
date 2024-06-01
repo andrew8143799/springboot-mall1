ddf38b40ef3631f0804d8670cc9efe3e168e2478
Andrew Yeh <andrew8143799@gmail.com>
2024-06-01 17:29:54 +0800

    初始化spring boot project

---
+++ src/main/java/com/andrewyeh/springbootmall1/SpringbootMall1Application.java
@@ -0,0 +1,13 @@
+package com.andrewyeh.springbootmall1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMall1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMall1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrewyeh/springbootmall1/SpringbootMall1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewyeh.springbootmall1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMall1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
