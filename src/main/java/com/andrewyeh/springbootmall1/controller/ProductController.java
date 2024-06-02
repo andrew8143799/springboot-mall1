7e36745329a6ecdb62e3afa4b7d5be56ba04233e
Andrew Yeh <andrew8143799@gmail.com>
2024-06-02 17:16:27 +0800

    商品功能 - 實作修改商品功能

--- src/main/java/com/andrewyeh/springbootmall1/controller/ProductController.java
+++ src/main/java/com/andrewyeh/springbootmall1/controller/ProductController.java
@@ -39,4 +39,23 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        Product product = productService.getProductById(productId);
+        //check if product exists
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }else {
+
+            productService.updateProduct(productId, productRequest);
+
+            Product updatedProduct = productService.getProductById(productId);
+
+            return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+        }
+
+    }
+
 }
